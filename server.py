5f9084bb9cd5d25c30e84f2d776d6e705bf7f337
Duncan Young <youngdun@arctic.cse.msu.edu>
2014-01-23 02:24:35 -0500

    my changes

--- server.py
+++ server.py
@@ -3,30 +3,33 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+	s = socket.socket()         # Create a socket object
+	host = socket.getfqdn() # Get local machine name
+	port = random.randint(8000, 9999)
+	s.bind((host, port))        # Bind to the port
 
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+	print 'Starting server on', host, port
+	print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+	s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-        # send a response
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send('Content-type: text/html\r\n')
-    c.send('\r\n')
-    c.send('<h1>Hello, world.</h1>')
-    c.send('This is ctb\'s Web server.')
-    c.close()
+	print 'Entering infinite loop; hit CTRL-C to exit'
+	while True:
+		# Establish connection with client.    
+		c, (client_host, client_port) = s.accept()
+		print 'Got connection from', client_host, client_port
+		handleconnection(c)
+def handle_connection(conn):
+			# send a response
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send('Content-type: text/html\r\n')
+    conn.send('\r\n')
+    conn.send('<h1>Hello, world.</h1>')
+    conn.send('This is DuncanCYoung\'s Web server.')
+    conn.close()
    
-    c.send('Thank you for connecting')
-    c.send("good bye.")
-    c.close()
+
+if __name__ == '__main__':
+    main()
